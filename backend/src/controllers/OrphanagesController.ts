5125bce4aa107046fcc5532f833cc6e197d9a181
guijun13 <guilhermejun13.grillo@gmail.com>
2020-10-17 01:55:48 -0300

    D2: validacao dos dados com yup

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -2,6 +2,7 @@ import { Request, response, Response } from 'express';
 import { getRepository, Index } from 'typeorm';
 import Orphanage from '../models/Orphanage';
 import orphanageView from '../views/orphanages_view';
+import * as Yup from 'yup';
 
 export default {
   async index(req: Request, res: Response){ // Listar os orfanatos
@@ -43,8 +44,8 @@ export default {
     const images = requestImages.map(image => {
       return { path: image.filename }
     })
-  
-    const orphanage = orphanagesRepository.create({
+    
+    const data = {
       name,
       latitude,
       longitude,
@@ -53,7 +54,28 @@ export default {
       opening_hours,
       open_on_weekends,
       images,
+    };
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required('Nome obrigatorio'),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required(),
+      about: Yup.string().required().max(300),
+      instructions: Yup.string().required(),
+      opening_hours: Yup.string().required(),
+      open_on_weekends: Yup.boolean().required(),
+      images: Yup.array(
+        Yup.object().shape({
+          path: Yup.string().required()
+        })
+      ),
+    })
+
+    await schema.validate(data, {
+      abortEarly: false,
     });
+
+    const orphanage = orphanagesRepository.create(data);
   
     await orphanagesRepository.save(orphanage);
   
