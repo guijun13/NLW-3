ddcfa52a002f690e54cb1f1e2c8cdc699082ff4c
guijun13 <guilhermejun13.grillo@gmail.com>
2020-10-30 01:08:37 -0300

    D4: criado navegacao entre telas

---
+++ mobile/src/pages/OrphanageDetails.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function OrphanageDetails(){
+  return(
+    <View>
+      <Text>Tela de detalhe de um orfanato</Text>
+    </View>
+  );
+}
